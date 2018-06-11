bddd0f465451e8a423ba2e67187d84127e66031e
sergiopoliveira <sergiopoliveira@outlook.com>
2018-06-11 10:35:56 +0100

    refactoring

--- src/main/java/sergio/springrestclientexamples/SpringRestClientExamplesApplication.java
+++ src/main/java/sergio/springrestclientexamples/SpringRestClientExamplesApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples;
+package sergio.springrestclientexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/sergio/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
+++ src/test/java/sergio/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples;
+package sergio.springrestclientexamples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
